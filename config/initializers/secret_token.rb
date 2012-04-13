6205b530c60f94466692ca39603ac1f5a63be655
Marc Heiligers <marc@eternal.co.za>
2012-04-13 18:48:32 +0200

    First commit - Rails app created, backbone and underscore libs added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudEdit::Application.config.secret_token = '3b0620a3023155939404715636d7a935e9c4ed8385f44e1774d250901ab541a4a4bb26d7ed7b354562db4765fc028a98f5e59f8f307d657916b1ba77c72de0ad'
